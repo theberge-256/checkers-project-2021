0c57f13c5fb55d79ede8602702b8198bc22371ce
Andrew T <floppian256@gmail.com>
2021-02-02 08:56:57 -0500

    Start to make class

--- gui.py
+++ gui.py
@@ -1,13 +1,23 @@
 import tkinter as tk
-
+# import controller thing from gui_code.py
 class CheckersApp(tk.Frame):
-    def __init__(self, master):
+    def __init__(self, master, controller_thing):
         super().__init__(master)
+
+        self.controller_thing = controller_thing
+
+        self.draw_gui()
+    
+    def draw_gui(self):
+        self.grid()
+        return
+
 
 
 root = tk.Tk()
 root.title("Checkers Application")
 
-app = CheckersApp(root)
+# app = CheckersApp(root, controller_thing)
+app = CheckersApp(root, False)
 
 root.mainloop()
