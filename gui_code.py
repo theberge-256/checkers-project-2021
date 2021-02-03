d218514e5e3a65deb1a3ec598d7bb0f84b5ea577
Nathan <nathankim012753@gmail.com>
2021-02-03 09:12:30 -0500

    Created the grid

--- gui_code.py
+++ gui_code.py
@@ -5,29 +5,37 @@ class ChessGui(Frame):
     def __init__(self, master):
     
     def set_coords:
-        #tihs needs to list every peice and apply a set of coordinates to every one.
+        #The coordinates need to assign a particular button, like button1 to the corresponding number. 
+        #Maybe like button[i] = [i] in list
+        #However, there also needs to be a way to replace each checkers circle button
+        #with the normal grid button. The button moving could work without having to use
+        #Math like (move up subtract 8 from i)
     def choose_peice:
-        coordinateone = self.piece1.get()
-        coordinatetwo = self.piece2.get()
+        coordinateloc = self.piece1.get()
         #piece1 and piece2 are also placeholders.
     def choose_location:
-        coordinatethree = self.location1.get()
-        coordinatefour = self.location4.get()
+        coordinatemove = self.location1.get()
         #location1 and location2 are placeholders for the actual widgets.
     
     def calculate_movement:
-        #here I need to implement a way to take the 2 pairs of coordinates
-        #and switch the peice at coordinate one to coordinate three, and change from coordinate 2 to coordinate 4.
-        #however, in order to do this, I need to create a function that creates a 8x8 grid. 
+        #There needs to be a way to track directional movements with the buttons. The only way I can think of is having a button have like [i] 
+        # and every time i is increased by 1, it moves right.
+        #Then we could implement the grid or something.
 
         
     def create_grid:
+        #this creates a list within lists, and assigns an area with a number. 
+        #The next step is too assign the buttons to the grid        
+        grid = []
 
-        #This grid would have to do an equation to find a specific location, like the coordinate A4 would have to move 
-        #4 up from A1. This would probably be done using a grid/coordinate plane.
-        #Another issue I realize is that for the Gui, circles and squares have to be created
-        #which we don't exactly know how to do yet.
-    
+        for row in range(8):
+            grid.append([])
+
+            for column in range(8):
+                grid[row].append(0)
+        
+        grid[1][4] = 1
+        
     def replace_piece:
         #while calculate_movement changes the coordinates of a peice, this needs to actively move the
         #checkers peice from one coordinate to another. The way I need to do this is probably by having the coords
