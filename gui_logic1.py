e25a42164d77db0359e3e53939e8c43b399e7356
Andrew T <floppian256@gmail.com>
2021-02-04 09:06:50 -0500

    Checker's logic start

---
+++ gui_logic1.py
@@ -0,0 +1,10 @@
+
+class CheckersLogic:
+    """
+        Class with all the logic, which will be used to store pieces
+        find out where pieces are allowed to go, and know the rule of the game
+        
+        This will some how be passed to the gui class in the future
+    """
+    def __init__(self):
+        print('Hello')
